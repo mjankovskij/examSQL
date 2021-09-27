0449c997272f91d4dc2eae09221dc9d19b6dc196
mjankovskij <70883106+mjankovskij@users.noreply.github.com>
2021-09-27 19:53:38 +0300

    schema, script

--- src/main/java/lt/codeacademy/entity/User.java
+++ src/main/java/lt/codeacademy/entity/User.java
@@ -1,6 +1,5 @@
 package lt.codeacademy.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,13 +10,8 @@ import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
-import javax.persistence.OneToOne;
 import javax.persistence.Table;
-import java.util.List;
 import java.util.Set;
 
 @Data
